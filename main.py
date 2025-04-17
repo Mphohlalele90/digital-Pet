bbcd2fcab54dcf6d4cfdd3c660d65d9385735563
Kesa1203 <kesasylvesterlee@gmail.com>
2025-04-17 16:37:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-class Pet:
-    # Constructor to initialize the pet's attributes
-    def __init__(self, name):
-        self.name = name  # Name of the pet
-        self.hunger = 15  # Hunger level (higher means hungrier)
-        self.energy = 10  # Energy level
-        self.happiness = 20  # Happiness level
-        self.tricks = []  # List of tricks the pet knows
+
